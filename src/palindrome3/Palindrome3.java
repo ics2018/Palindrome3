ef8c126314d85f438725eb423415032c60759bef
rkaune <rkaune@7HPL6Y1.Student.UGDSB.ED>
2018-09-11 15:16:59 -0400

    This is the Palindrome 3 code from Unit 1 Activity 2

---
+++ src/palindrome3/Palindrome3.java
@@ -0,0 +1,34 @@
+package palindrome3;
+
+import javax.swing.*;
+
+public class Palindrome3 {
+
+    public static void main(String[] args) {
+        String originalWord, word, backwardsWord, palindromes = "";
+        int spacePos, pos, palCounter = 0;
+
+        //get the word from the user
+        originalWord = JOptionPane.showInputDialog(
+                "Words that are the same forwards and backwards are called palindromes.\n"
+                + "This program determines if a phrase is a palindrome.\n\n"
+                + "Enter a phrase(do not include a punctuation mark):");
+
+
+        //remove the spaces from the sentence
+        word = originalWord.replaceAll(" ", "");
+
+        //Reverse the sentence
+        backwardsWord = "";
+        for (pos = word.length() - 1; pos >= 0; pos--) {
+            backwardsWord += word.charAt(pos);
+        }
+
+        //Compare the words
+        if (word.equalsIgnoreCase(backwardsWord)) {
+            JOptionPane.showMessageDialog(null, originalWord + " IS a palindrome.");
+        } else {
+            JOptionPane.showMessageDialog(null, originalWord + " IS NOT a palindrome.");
+        }
+    }
+}
